fa942403990a45cd0307848e7bb2db5ac788de0d
GuihCFerreira <guihhh231@gmail.com>
2024-08-06 21:20:56 -0300

    feat: add header, header components and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Button>Teste</Button>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p>Segunda Feira, 05 de Agosto</p>
+
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full h-[150px] mt-6">
+          <Image
+            alt="Agende com os melhores"
+            src="/banner-01.png"
+            fill
+            className="object-cover rounded-xl"
+          />
+        </div>
+      </div>
     </div>
   );
 }
