431b30e559d3bee441cff6e7d0b86f5fd6e63647
GuihCFerreira <guihhh231@gmail.com>
2024-08-09 22:56:03 -0300

    chore: add next auth setup

--- src/app/_components/sidebar-sheet.tsx
+++ src/app/_components/sidebar-sheet.tsx
@@ -1,6 +1,12 @@
 import { Button } from "./ui/button";
 import Image from "next/image";
-import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+} from "lucide-react";
 import {
   SheetContent,
   SheetTrigger,
@@ -12,6 +18,13 @@ import {
 import { quickSearchOptions } from "../_constants/search";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+} from "./ui/dialog";
+import { DialogTitle, DialogTrigger } from "@radix-ui/react-dialog";
 
 const SidebarSheet = () => {
   return (
@@ -20,15 +33,37 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 py-5 border-b border-solid">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 py-5 border-b border-solid">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size={"icon"}>
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conect-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant={"secondary"} className="gap-2 font-bold">
+              <Image src={"/google.svg"} width={18} height={18} alt="Google" />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1719095150877-a1085076c726?q=80&w=1966&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
 
         <div>
           <p className="font-bold">Felipe Rocha</p>
           <p className="text-xs">felipe@fullstackclub.io</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 py-5 border-b border-solid">
