4101306d6e6ed406957895bb326031eed2ba45e7
GuihCFerreira <guihhh231@gmail.com>
2024-08-12 23:21:38 -0300

    feat: ensure only authenticated user can book

--- src/app/_actions/create-booking.ts
+++ src/app/_actions/create-booking.ts
@@ -1,17 +1,23 @@
 "use server";
 
+import { getServerSession } from "next-auth";
 import { revalidatePath } from "next/cache";
+import { authOptions } from "../_lib/auth";
 import { db } from "../_lib/prisma";
 
 interface CreateBookingParams {
   serviceId: string;
-  userId: string;
   date: Date;
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
+  const user = await getServerSession(authOptions);
+  if (!user) throw new Error("Usuário não autenticado");
   await db.booking.create({
-    data: params,
+    data: {
+      ...params,
+      userId: (user.user as any).id,
+    },
   });
   revalidatePath("/barbershops/{id}");
 };
