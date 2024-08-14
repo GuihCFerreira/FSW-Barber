ad3e8cbe4e6af05e14254253db9d9b20dff6bd18
GuihCFerreira <guihhh231@gmail.com>
2024-08-13 22:54:00 -0300

    feat: ensure booking can be viewed

---
+++ src/app/_components/booking-summary.tsx
@@ -0,0 +1,44 @@
+import { format } from "date-fns";
+import { Card, CardContent } from "./ui/card";
+import { ptBR } from "date-fns/locale";
+
+const BookingSummary = () => {
+  return (
+    <>
+      <Card>
+        <CardContent className="p-3 space-y-2">
+          <div className="flex justify-between items-center">
+            <h2 className="font-bold">{service.name}</h2>
+            <p className="text-sm font-bold">
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(Number(service.price))}
+            </p>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <h2 className="text-sm">Data</h2>
+            <p className="text-sm">
+              {format(selectedDay, "d 'de' MMMM", {
+                locale: ptBR,
+              })}
+            </p>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <h2 className="text-sm">Horário</h2>
+            <p className="text-sm">{selectedTime}</p>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <h2 className="text-sm">Barbearia</h2>
+            <p className="text-sm">{barbershop.name}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default BookingSummary;
